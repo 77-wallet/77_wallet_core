65fbe285b81693dda5c3ba015c544435254b7f37
wenjing <wenjing@company.com>
2024-12-07 19:31:42 +0800

    chore: error

--- wallet-utils/src/file_func.rs
+++ wallet-utils/src/file_func.rs
@@ -18,7 +18,7 @@ pub fn recreate_dir_all<P: AsRef<Path>>(path: P) -> Result<(), crate::Error> {
     Ok(fs::create_dir_all(path)?)
 }
 
-pub fn copy_file<P: AsRef<Path>>(src: P, dst: P) -> Result<(), std::io::Error> {
+pub fn copy_file<P: AsRef<Path>>(src: P, dst: P) -> Result<(), crate::Error> {
     std::fs::copy(src, dst)?;
     Ok(())
 }
