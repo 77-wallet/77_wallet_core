8ca722ba155ed5f2e71ce3ae8bc94ede8960e649
sereins0618 <sereins0618@gmail.com>
2025-01-09 10:06:49 +0800

    chore:tron chain parameter visible

--- wallet-chain-interact/src/tron/protocol/mod.rs
+++ wallet-chain-interact/src/tron/protocol/mod.rs
@@ -1,6 +1,7 @@
 pub mod account;
 pub(crate) mod block;
-pub(crate) mod chain_parameter;
+pub mod chain_parameter;
+pub use chain_parameter::ChainParameter;
 pub(crate) mod protobuf;
 pub(crate) mod receipt;
 pub(crate) mod transaction;
