117ffd84ec74ac8591d1f3ab5e8bcb25936cfc82
peterwang <peterwang@peterwangdemacbook-pro.tail769b0e.ts.net>
2025-01-14 15:00:54 +0800

    delete ltc useless code

--- wallet-chain-interact/src/ltc/protocol/transaction.rs
+++ wallet-chain-interact/src/ltc/protocol/transaction.rs
@@ -1,6 +1,9 @@
 use std::fmt::Debug;
 
-use crate::ltc::{consts::BTC_DECIMAL, utxos::Utxo};
+use crate::{
+    btc::protocol::transaction::AddressInfo,
+    ltc::{consts::BTC_DECIMAL, utxos::Utxo},
+};
 use serde::{Deserialize, Serialize};
 use wallet_types::chain::address::r#type::LtcAddressType;
 use wallet_utils::unit;
