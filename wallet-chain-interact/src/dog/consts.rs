8e2fd9b4528ec17f76c8ccd41393a6b5cb9a2b9a
peterwang <peterwang@peterwangdeMacBook-Pro.local>
2025-01-22 18:34:11 +0800

    expend max_fee_rate of doge

--- wallet-chain-interact/src/dog/consts.rs
+++ wallet-chain-interact/src/dog/consts.rs
@@ -2,6 +2,6 @@ pub const DOG_DECIMAL: u8 = 8;
 pub const DOG_VALUE: f64 = 100_000_000.0;
 pub const FEE_RATE: u8 = 1;
 // 手续费最大费率
-pub const MAX_FEE_RATE: u64 = 200;
+pub const MAX_FEE_RATE: u64 = 200_000_000;
 // 手续费放大的值
 pub const EXPEND_FEE_RATE: u64 = 2;
