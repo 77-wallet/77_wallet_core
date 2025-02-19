c16b9d3572e0e11724ec8baf10bd1de67691bc86
gin <gin@github.com>
2025-02-19 21:14:14 +0800

    fix: coins_bip39::WordlistError::InvalidWord do not return word

--- wallet-core/src/error/mod.rs
+++ wallet-core/src/error/mod.rs
@@ -16,6 +16,9 @@ impl From<coins_bip39::MnemonicError> for Error {
     fn from(value: coins_bip39::MnemonicError) -> Self {
         let msg = match value {
             coins_bip39::MnemonicError::InvalidPhrase(_) => "the phrase is invalid".to_string(),
+            coins_bip39::MnemonicError::WordlistError(
+                _e @ coins_bip39::WordlistError::InvalidWord(_),
+            ) => "the word is invalid".to_string(),
             _ => value.to_string(),
         };
         Error::Mnemonic(msg)
