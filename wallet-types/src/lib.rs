efbb7e707ff151d858d540fa99d5381eea7efcb6
gin <gin@github.com>
2025-06-03 19:38:03 +0800

    fix: rust decimal add feature serde-with-float

--- wallet-types/src/lib.rs
+++ wallet-types/src/lib.rs
@@ -4,3 +4,37 @@ pub mod constant;
 pub mod error;
 pub mod valueobject;
 pub use error::Error;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct BalanceTest {
+    #[serde(with = "rust_decimal::serde::float")]
+    balance: Decimal,
+}
+
+#[test]
+fn test_deserialize_and_serialize_decimal() {
+    let json_data = r#"{
+        "balance": 0.002906173
+    }"#;
+
+    // 反序列化 JSON -> BalanceTest
+    let result: Result<BalanceTest, _> = serde_json::from_str(json_data);
+    match result {
+        Ok(parsed) => {
+            println!("Parsed balance: {}", parsed.balance);
+            assert_eq!(
+                parsed.balance,
+                Decimal::from_str_exact("0.002906173").unwrap()
+            );
+
+            // 序列化 BalanceTest -> JSON
+            let serialized = serde_json::to_string(&parsed).unwrap();
+            println!("Serialized struct: {}", serialized);
+        }
+        Err(err) => {
+            panic!("Failed to deserialize: {}", err);
+        }
+    }
+}
