3c90a2eaf34c95c2ca1d81a4aefa6f5dcc1e7c74
gin <gin@github.com>
2025-06-05 16:47:20 +0800

    chore: add deserialize_empty_string_as_none

--- wallet-utils/src/serde_func.rs
+++ wallet-utils/src/serde_func.rs
@@ -77,6 +77,14 @@ where
         .ok_or_else(|| serde::de::Error::custom("Failed to convert f64 to Decimal"))
 }
 
+pub fn deserialize_empty_string_as_none<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let opt: Option<String> = Option::deserialize(deserializer)?;
+    Ok(opt.filter(|s| !s.is_empty()))
+}
+
 pub fn serialize_lowercase<S>(value: &str, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: serde::Serializer,
