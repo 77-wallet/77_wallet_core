086091adaba7a001b5a2f1fd737e712f63f0c014
gin <gin@github.com>
2025-07-28 19:27:42 +0800

    chore: export keystore::generator::

--- wallet-crypto/src/lib.rs
+++ wallet-crypto/src/lib.rs
@@ -17,7 +17,7 @@ pub type KeystoreJson = crypto::encrypted_json::encrypted::EncryptedJson;
 pub use crypto::encrypted_json::cryptor::EncryptedJsonGenerator;
 pub use kdf::factory::KdfAlgorithm;
 pub use keystore::builder::{KeystoreBuilder, RecoverableData};
-pub use keystore::generator::KeystoreJsonGenerator;
+pub use keystore::generator::{KeystoreJsonDecryptor, KeystoreJsonGenerator};
 // pub use wallet_tree::wallet_tree::WalletTreeStrategy;
 
 fn generate_random_bytes<R: rand::Rng + rand::CryptoRng>(rng: &mut R, len: usize) -> Vec<u8> {
